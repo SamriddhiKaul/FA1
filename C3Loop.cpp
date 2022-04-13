a874616978c93b4ddbc30cde6c30d848c699a07c
SamriddhiKaul <102215422+SamriddhiKaul@users.noreply.github.com>
2022-04-13 14:08:08 +0530

    Add files via upload

---
+++ C3Loop.cpp
@@ -0,0 +1,44 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the value of n"<<endl;
+//     cin>>n;
+//     int sum=0;
+//     for(int i=1;i<=n;i++){
+//         sum=sum+i;
+//     }
+//     cout<<sum<<endl;
+//     return 0;
+// }
+
+// *Keep taking input until user gives a negative number 
+// When you enter any negative number , it will terminate the output
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"keep on entering numbers"<<endl;
+//     cin>>n;
+//     while(n>0){
+//         cout<<n<<endl;
+//         cin>>n;
+//     }
+// return 0;
+// }
+
+// Using dowhile , Ek baar code execute hojaega , baad me terminate hoga! 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"keep on entering numbers"<<endl;
+//     cin>>n;
+//     do{
+//         cout<<n<<endl;
+//         cin>>n;
+//     }while(n>0);
+// return 0;
+// }
+
+
